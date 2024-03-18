9be5297e0a4cc4f92ff122a9fe07a6e66736256f
LENV <technicalpriyanshutaru@gmail.com>
2024-03-18 22:05:41 +0530

    github tutorial

---
+++ lib/day_one_tutorial.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class DayOneTutorial extends StatefulWidget {
+  const DayOneTutorial({super.key});
+
+  @override
+  State<DayOneTutorial> createState() => _DayOneTutorialState();
+}
+
+class _DayOneTutorialState extends State<DayOneTutorial> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}

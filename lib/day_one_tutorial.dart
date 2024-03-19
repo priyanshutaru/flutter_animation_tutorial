4a424cdb2c5304ce292a829c3cd4f0464c9e759c
LENV <technicalpriyanshutaru@gmail.com>
2024-03-19 23:40:22 +0530

    animation start

--- lib/day_one_tutorial.dart
+++ lib/day_one_tutorial.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_field
 
 import 'package:flutter/material.dart';
+import 'dart:math' show pi;
 
 class DayOneTutorial extends StatefulWidget {
   const DayOneTutorial({super.key});
@@ -9,15 +10,26 @@ class DayOneTutorial extends StatefulWidget {
   State<DayOneTutorial> createState() => _DayOneTutorialState();
 }
 
-class _DayOneTutorialState extends State<DayOneTutorial> {
+class _DayOneTutorialState extends State<DayOneTutorial>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 3),);
+
+        _animation = Tween<double>(begin: 1,end: 2 * pi).animate(_controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: [
-          
-        ],
+        children: [],
       ),
     );
   }

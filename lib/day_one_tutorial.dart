0a52cd49989ab11e4cabe57039f609b284c0b38c
LENV <technicalpriyanshutaru@gmail.com>
2024-03-20 08:51:03 +0530

    first lecture desing done.

--- lib/day_one_tutorial.dart
+++ lib/day_one_tutorial.dart
@@ -18,18 +18,99 @@ class _DayOneTutorialState extends State<DayOneTutorial>
   @override
   void initState() {
     super.initState();
-    _controller =
-        AnimationController(vsync: this, duration: Duration(seconds: 3),);
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
 
-        _animation = Tween<double>(begin: 1,end: 2 * pi).animate(_controller);
+    _animation = Tween<double>(begin: 0.0, end: 2 * pi).animate(_controller);
+
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [],
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Column(
+              children: [
+                                SizedBox(height: 100,),
+                Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()..rotateX(_animation.value),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()..rotateY(_animation.value),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()..rotateZ(_animation.value),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 100,),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+       scaffoldBackgroundColor: Colors.black
       ),
       home: DayOneTutorial(),
     );
